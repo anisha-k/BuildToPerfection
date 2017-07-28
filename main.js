8337cc7a73a2ba0cf92f32d414f7feb9a3c2dce4
Anisha Kapoor <anishakapoor1012@gmail.com>
2017-07-28 17:36:25 +0530

    Updates/Enhancements

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function addNote() {
 	var note = document.createElement("textarea");
 	var colorList = ["#F0FF33", "#FFE933", "#33FFB2", "#33FFE0", "#EC33FF", "#FF33F6", "#DFBED4"];
 	note.style.backgroundColor = colorList[Math.floor(Math.random() * colorList.length)];
-	console.log("x: " + rowRef.rowIndex + ", y: " + cellRef.cellIndex + ", color: " + note.style.backgroundColor);
+	console.log("note x: " + rowRef.rowIndex + ", y: " + cellRef.cellIndex + ", color: " + note.style.backgroundColor);
 	var date = new Date();
 	note.id = "note"+date.getDate()+date.getHours()+date.getMinutes()+date.getSeconds()+date.getMilliseconds();
 	note.ondblclick = addNote;
@@ -22,15 +22,17 @@ function addDate(noteId) {
 	console.log("noteId addDate: " + noteId);
 	var noteRef = document.getElementById(noteId);
 	var notesRef = document.getElementById("notes");
-	var dateRowRef = notesRef.insertRow(noteRef.parentNode.parentNode.rowIndex);
+	var dateRowRef = notesRef.insertRow(noteRef.parentNode.parentNode.rowIndex+1);
 	var dateCellRef = dateRowRef.insertCell(noteRef.parentNode.cellIndex);
+	console.log("date x: " + dateRowRef.rowIndex + ", y: " + dateCellRef.cellIndex);
 	var noteDateLbl = document.createElement("LABEL");
 	noteDateLbl.id = "date"+noteId.substring(4);
 	console.log("noteDateLblId: " + noteDateLbl.id);
 	noteDateLbl.style.backgroundColor = noteRef.style.backgroundColor;
 	noteDateLbl.innerHTML = "Last modified: "+ (new Date()).toLocaleString();
 	dateCellRef.appendChild(noteDateLbl);
-	//console.log("date x: " + noteDateLbl.parentNode.parentNode.rowIndex + ", y: " + noteDateLbl.parentNode.cellIndex + ", color: " + noteRef.style.backgroundColor);
+	console.log("note x: " + noteRef.parentNode.parentNode.rowIndex + ", y: " + noteRef.parentNode.cellIndex);
+	console.log("date x: " + noteDateLbl.parentNode.parentNode.rowIndex + ", y: " + noteDateLbl.parentNode.cellIndex);
 }
 function updateDate(noteId) {
 	console.log("noteId updateDate: " + noteId);
